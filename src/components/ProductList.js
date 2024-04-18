10b5341ee52ac820acfad6a6bc46bc3e885fea4a
Thomas Maghanga <142288589+tom-maghanga@users.noreply.github.com>
2024-04-18 15:16:58 +0300

    Added Button to view in stocka nd all prodcuts

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,26 +2,31 @@ import React, { useEffect, useState } from 'react'
 
 function ProductList() {
     const [products, setProducts] = useState([]);
-    console.log(products);
+    const[url, setUrl] = useState("http://localhost:8000/products");
 
-    useEffect(()=>{
-      fetch('http://localhost:8000/products')
-    .then(response => response.json())
-    .then( data => setProducts(data));
-    }, []);
+     
+    useEffect(() => {
+      fetch(url)
+      .then(response => response.json())
+      .then(data => setProducts(data));
+  }, [url]);
 
   return (
     <section>
-      {products.map((product) => (
-        <div className='card' key={product.id}>
-            <p className='id'>{product.id}</p>
-            <p className='name'>{product.name}</p>
-            <p className='info'>
-              <span>${product.price}</span>
-              <span className={product.in_stock ?  "In Stock" : "Unavailable"}>{product.in_stock ?  "In Stock" : "Unavailable"}</span>
-            </p>
-        </div>
-      ))}
+      <div className='filter'>
+        <button onClick={() => setUrl("http://localhost:8000/products")}>All</button>
+        <button onClick={() => setUrl("http://localhost:8000/products?in_stock=1")}>In Stock</button>
+      </div>
+      { products.map((product) => (
+            <div className="card" key={product.id}>
+                <p className="id">{product.id}</p>
+                <p className="name">{product.name}</p>
+                <p className="info">
+                    <span>${product.price}</span>
+                    <span className={product.in_stock ? "instock" : "unavailable"}>{product.in_stock ? "In Stock" : "Unavailable"}</span>
+                </p>
+            </div>            
+        )) }
     </section>
   )
 }
